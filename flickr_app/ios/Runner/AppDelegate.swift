9af7927a802a2d583416a4fdfe50374972376f93
S-EmreCat <emre__cat@outlook.com>
2021-07-30 00:54:04 +0300

    google maps no api key

--- flickr_app/ios/Runner/AppDelegate.swift
+++ flickr_app/ios/Runner/AppDelegate.swift
@@ -3,13 +3,14 @@ import Flutter
 import GoogleMaps
 
 
+
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool { 
-    GMSServices.provideAPIKey("AIzaSyA5pOsKfAbuW51fqsHSvqvymEhPynKypKA")
+    GMSServices.provideAPIKey("")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
